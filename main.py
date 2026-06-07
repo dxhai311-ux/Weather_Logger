3183c1019c89a706427342323b6c8770a3eff91e
Xuan Hai <dxhai311@gmail.com>
2026-06-07 15:52:13 +0700

    add network error handling

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from dotenv import load_dotenv
 from db import get_engine
 from sqlalchemy import text
 
+engine = get_engine()
+load_dotenv()
+
 def save_weather(city, temp, humidity, description):
-    engine = get_engine()
     #text : Bọc câu SQL thành object mà SQLAlchemy 2.0 hiểu được,
     #nếu không có text thì nó sẽ hiểu là string bình thường
     with engine.connect() as conn:
@@ -27,8 +29,6 @@ def save_weather(city, temp, humidity, description):
     print("Đã lưu vào database!")
 #execute(tham số 1, tham số 2) → thực thi câu SQL đã chuẩn bị
 if __name__ == "__main__":
-    load_dotenv()
-
     api_key = os.getenv('API_KEY')
     city = input("Nhập tên thành phố (tiếng Anh): ").title()
 
@@ -37,8 +37,13 @@ if __name__ == "__main__":
     #appid → API key để xác thực
     #lang=vi → trả về mô tả thời tiết bằng tiếng Việt
 
-    response = requests.get(url) #gửi yêu cầu đến API và nhận phản hồi
-    data = response.json() #chuyển đổi phản hồi từ API (định dạng JSON) thành một đối tượng Python (thường là dict hoặc list) để dễ dàng truy cập và xử lý dữ liệu.
+    try:
+        response = requests.get(url)
+        response.raise_for_status() #nếu có lỗi HTTP sẽ ném ra exception, và chúng ta sẽ bắt nó trong except
+        data = response.json() #chuyển dữ liệu JSON thành dict của Python
+    except requests.exceptions.RequestException as e:
+        print(f"Lỗi khi gọi API: {e}")
+        exit()
 
     if data.get('cod') == '404': 
         #404: không tìm thấy thành phố
@@ -57,3 +62,9 @@ if __name__ == "__main__":
         print(f"Độ ẩm: {humidity}%")
         print(f"Mô tả: {description}")
         save_weather(city, temp, humidity, description)
+
+#====Cấu trúc====
+#Tạo engine : Lúc này Python chưa kết nối thật — chỉ tạo ra "bản đồ" biết database ở đâu.
+#engine.connect() : Mở kết nối thật sự với database, giống như mở cửa vào nhà.
+#conn.execute() : Python gửi câu SQL qua kết nối → PostgreSQL nhận → xử lý → thêm data vào bảng.
+#conn.commit() : PostgreSQL lưu data thật sự vào ổ cứng.
